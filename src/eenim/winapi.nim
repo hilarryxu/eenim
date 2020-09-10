6753059cc2fb835814fa8b359732105069ce9680
Larry Xu <hilarryxu@gmail.com>
2020-09-10 18:01:43 +0800

    refactor: remove unused code in winapi.nim

--- src/eenim/winapi.nim
+++ src/eenim/winapi.nim
@@ -46,20 +46,5 @@ const
 
   MB_OK* = 0
 
-when defined(useWinUnicode):
-  proc WC*(s: string): LPCWSTR =
-    if s.len == 0: return cast[LPCWSTR](0)
-    let x = newWideCString(s)
-    result = cast[LPCWSTR](x)
-else:
-  template WC*(s: string): cstring = s.cstring
-
-when defined(useWinUnicode):
-  proc MessageBoxW*(hWnd: HWND, lpText: WideCString, lpCaption: WideCString, uType: WINUINT): cint {.
-    importc: "MessageBoxW", dynlib: "user32", stdcall.}
-
-proc MessageBoxA*(hWnd: HWND, lpText: cstring, lpCaption: cstring, uType: WINUINT): cint {.
-  importc: "MessageBoxA", dynlib: "user32", stdcall.}
-
 proc OutputDebugStringA*(lpOutputString: cstring) {.
   importc: "OutputDebugStringA", dynlib: "kernel32", stdcall.}
